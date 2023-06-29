70d9309884ce8b61c67500287ad88c672da20154
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-06-29 14:08:44 +0300

    application record corrected

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,21 @@
-class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
-    def create
-      lease = Lease.new(lease_params)
-  
-      if lease.save
-        render json: lease
-  
+class LeasesController < ApplicationController
+  def create
+    lease = Lease.new(lease_params)
+
+    if lease.save
+      render json: lease, status: :created
+    else
+      render json: lease.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # Your destroy action logic here
+  end
+
+  private
+
+  def lease_params
+    params.require(:lease).permit(:rent)
+  end
+end
